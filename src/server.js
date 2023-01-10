d13c6b149f2018d71071b4c33baa1ddfca1b7bba
Mattia Capelli <mattia@mattiacapelli.it>
2023-01-10 18:52:11 +0100

    🐝Create the server and some routes

--- src/server.js
+++ src/server.js
@@ -38,3 +38,103 @@ function getIP(req) {
 
 //Clear console
 console.clear();
+
+var app = express();
+
+app.listen(config.port, function(err) {
+    if(err) {
+        log('error', 'Error starting server');
+    } else {
+        log('success', 'Server started on port ' + config.port + '\n' + chalk.underline('http://localhost:' + config.port));
+    }
+});
+
+app.get('/', function (req, res) {
+    log('info', 'Express > Request received from ' + getIP(req));
+    res.json({
+        code: 200,
+        message: 'OK',
+    });
+});
+
+app.get('/os', function (req, res) {
+log('info', 'Express > Request received from ' + getIP(req));
+    res.json({
+        code: 200,
+        message: 'OK',
+        data: {
+            hostname: os.hostname(),
+            type: os.type(),
+            platform: os.platform(),
+            arch: os.arch(),
+            release: os.release(),
+            uptime: os.uptime(),
+        }
+    });
+});
+
+app.get('/os/hostname', function (req, res) {
+log('info', 'Express > Request received from ' + getIP(req));
+    res.json({
+        code: 200,
+        message: 'OK',
+        data: {
+            hostname: os.hostname()
+        }
+    });
+});
+
+app.get('/os/type', function (req, res) {
+log('info', 'Express > Request received from ' + getIP(req));
+    res.json({
+        code: 200,
+        message: 'OK',
+        data: {
+            type: os.type()
+        }
+    });
+});
+
+app.get('/os/platform', function (req, res) {
+log('info', 'Express > Request received from ' + getIP(req));
+    res.json({
+        code: 200,
+        message: 'OK',
+        data: {
+            platform: os.platform()
+        }
+    });
+});
+
+app.get('/os/arch', function (req, res) {
+log('info', 'Express > Request received from ' + getIP(req));
+    res.json({
+        code: 200,
+        message: 'OK',
+        data: {
+            arch: os.arch()
+        }
+    });
+});
+
+app.get('/os/release', function (req, res) {
+log('info', 'Express > Request received from ' + getIP(req));
+    res.json({
+        code: 200,
+        message: 'OK',
+        data: {
+            release: os.release()
+        }
+    });
+});
+
+app.get('/os/uptime', function (req, res) {
+log('info', 'Express > Request received from ' + getIP(req));
+    res.json({
+        code: 200,
+        message: 'OK',
+        data: {
+            uptime: os.uptime()
+        }
+    });
+});

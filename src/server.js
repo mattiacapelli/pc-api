7e72a2999eed919461ed53324d7caf401d387a07
Mattia Capelli <mattia@mattiacapelli.it>
2023-01-10 18:43:45 +0100

    📄Create the loglevel var

--- src/server.js
+++ src/server.js
@@ -2,3 +2,12 @@ var express = require('express');
 var chalk = require('chalk');
 var os = require('os');
 
+var loglevel = 
+{
+    error: chalk.bold.red,
+    warn: chalk.yellow,
+    success: chalk.bold.green,
+    debug: chalk.blue,
+    warning: chalk.yellow,
+    info: chalk.blue
+}
